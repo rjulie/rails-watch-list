339f5e4757dd268d808a9bf65fbdc0aadd285cb7
Julie Reynal <rjulie.pro@gmail.com>
2022-02-17 16:18:43 +0100

    list controller silo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+
+
+movies.each do |movie|
+
+  poster_url_complete = "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
+
+  new_movie = Movie.new(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: poster_url_complete,
+    rating: movie['vote_average'],
+  )
+
+  new_movie.save!
+
+end
+
+puts "Finished!"
